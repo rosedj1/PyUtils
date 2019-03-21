50f8054dc57e8d39d4e5db4b8229b30c62334c39
Jake <rosedj1@gmail.com>
2019-03-21 09:46:51 -0400

    Minor modifications

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # PyUtils 
 
 This repo contains frequently-used code which is used for copying files, etc.
+
 It can be used in a variety of projects.
--- fileUtils.py
+++ fileUtils.py
@@ -1,12 +1,5 @@
-##__________________________________________________________________||
-def mkdir_p(path):
-    # http://stackoverflow.com/questions/600268/mkdir-p-functionality-in-python
-    try:
-        os.makedirs(path)
-    except OSError as exc: # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else: raise
+import os
+from shutil import copy2
 
 ##__________________________________________________________________||
 ### CAN PROBABLY JUST USE: from shutil import copyfile ... LOL
@@ -17,7 +10,7 @@ def copyFile(inDir,fileName,outDir):
     fileName    = file name
     '''
     ## Make directory if it doesn't exist
-    mkdir_p(outDir)
+    makeDirs(outDir)
     ## Check if file already exists in outDir
     if not os.path.exists(outDir+fileName):
         copy2(inDir+fileName, outDir+fileName)
@@ -26,3 +19,13 @@ def copyFile(inDir,fileName,outDir):
 def makeDirs(dir):                       
     if not os.path.exists(dir):          
         os.makedirs(os.path.abspath(dir))
+
+##__________________________________________________________________||
+#def mkdir_p(path):
+#    # http://stackoverflow.com/questions/600268/mkdir-p-functionality-in-python
+#    try:
+#        os.makedirs(path)
+#    except OSError as exc: # Python >2.5
+#        if exc.errno == errno.EEXIST and os.path.isdir(path):
+#            pass
+#        else: raise
